0e2f8845f71d7a00c9040166e04a70a540446972
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-29 11:11:05 +0530

    add

---
+++ one.java
@@ -0,0 +1,12 @@
+class one
+{
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = 3;
+        int result = a + b;
+        {
+            System.out.println(result);
+        }
+    }
+}
