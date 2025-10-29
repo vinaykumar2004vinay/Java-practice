dee739976326dd80a7b39c7ff3e95d7b43bbecb0
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-29 12:01:50 +0530

    Packages

---
+++ Packages/Example.java
@@ -0,0 +1,12 @@
+package Packages;
+public class Example
+{
+    public void SayHello()
+    {
+        System.out.println("My Name Is Vinay");
+    }
+    public static void main(String args[])
+    {
+
+    }
+}
---
+++ Packages/Example2.java
@@ -0,0 +1,9 @@
+import Packages.Example;
+public class Example2
+{
+    public static void main(String args[])
+    {
+        Example e = new Example();
+        e.SayHello();
+    }
+}
