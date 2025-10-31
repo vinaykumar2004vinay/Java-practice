f7a8cee83a36443c41e0ae82a6ff9659dfd0a3da
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-31 15:45:55 +0530

    Java

---
+++ Variables/one.java
@@ -0,0 +1,13 @@
+public class one
+{
+    public static void main(String args[])
+    {
+        String powerstar = "Pavan Kalyan";  
+        String stylishstar = "Allu Arjun";
+        int rebelstar = 1;
+
+        System.out.println(powerstar);
+        System.out.println(stylishstar);
+        System.out.println(rebelstar);
+    }
+}
