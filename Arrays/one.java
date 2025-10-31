3245c80f963e6d905eeaf587d95ca619cb056d7b
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-31 16:27:39 +0530

    JAVA

---
+++ Arrays/one.java
@@ -0,0 +1,19 @@
+class one
+{
+    public static void main(String args[])
+    {
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int num4 = 4;
+        int num5 = 5;
+        int num6 = 6;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+    }
+}
---
+++ Arrays/two.java
@@ -0,0 +1,14 @@
+class two
+{
+    public static void main(String args[])
+    {
+        int number[] = {1,2,3,4,5,6};
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+        System.out.println(number[4]);
+        System.out.println(number[5]);
+    }
+}
