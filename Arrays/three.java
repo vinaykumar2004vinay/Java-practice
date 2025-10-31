8463c01aadfabb1b53081586dd1e326a1275bb84
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-31 16:38:40 +0530

    JAVA

---
+++ Arrays/three.java
@@ -0,0 +1,13 @@
+class three
+{
+    public static void main(String args[])
+    {
+        String Words[] = {"Rahul" , "Vinay" , "Kumar" , "Pavan" , "Arjun"};
+
+        System.out.println(Words[0]);
+        System.out.println(Words[1]);
+        System.out.println(Words[2]);
+        System.out.println(Words[3]);
+        System.out.println(Words[4]);
+    }
+}
