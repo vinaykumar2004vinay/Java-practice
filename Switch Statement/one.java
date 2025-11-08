6c761e694be8520948b88c361f80f0d93164717f
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-11-08 10:54:08 +0530

    JAVA

---
+++ Switch Statement/one.java
@@ -0,0 +1,24 @@
+class one
+{
+    public static void main(String args[])
+    {
+        int n = 7;
+        switch(n)
+        {
+            case 1 :
+                System.out.println("Monday");
+            case 2 :
+                System.out.println("Tuesday");
+            case 3 :
+                System.out.println("Wednesday");
+            case 4 :
+                System.out.println("Thursday");
+            case 5 :
+                System.out.println("Friday");
+            case 6 :
+                System.out.println("Saturday");
+            case 7 :
+                System.out.println("Sunday");
+        }
+    }
+}
