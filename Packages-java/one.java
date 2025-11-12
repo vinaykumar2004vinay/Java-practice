05530fce4fdfe19eddc4a20745260fcca02bf96f
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-11-12 14:35:44 +0530

    JAVA

---
+++ Packages-java/one.java
@@ -0,0 +1,13 @@
+import tools.*;
+import java.lang.*;
+
+public class one
+{
+    public static void main(String[] args)
+    {
+        Calculator obj = new Calculator();
+        AdvCalc obj1 = new AdvCalc();
+
+        System.out.println();
+    }
+}
---
+++ Packages-java/tools/AdvCalc.java
@@ -0,0 +1,13 @@
+package tools;
+public class AdvCalc extends Calculator
+{
+    public int multi(int n1,int n2)
+    {
+        return n1*n2;
+    }
+
+    public int div(int n1,int n2)
+    {
+        return n1/n2;
+    }
+}
