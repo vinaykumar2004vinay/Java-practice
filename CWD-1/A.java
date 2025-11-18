b4d0376787c97d9f1157483e4ed70ce87498c7b8
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-11-18 11:12:51 +0530

    JAVA

---
+++ CWD-1/A.java
@@ -0,0 +1,8 @@
+package pack1;
+public class A
+{
+    public void display()
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ CWD-1/B.java
@@ -0,0 +1,10 @@
+package pack2;
+import pack1.A;
+public class B
+{
+    public static void main(String[] args)
+    {
+        A obj = new A();
+        obj.display();
+    }
+}
